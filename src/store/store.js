172a94e841201105b1afc0b700beb8346c6d8de3
Biro Carol <birocarol@gmail.com>
2023-02-09 11:30:27 +0200

    Added a spinner while catogory is loading using redux thunk

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 // import { loggerMiddleware } from './middleware/logger';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
@@ -15,7 +16,11 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer); 
 
-const middleWares =[process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middleWares =[
+    process.env.NODE_ENV !== 'production' && logger, 
+    thunk
+].filter(Boolean);
+
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
 
